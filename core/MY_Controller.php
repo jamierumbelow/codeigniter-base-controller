938dd4eb61470b21d66e6830599205acc0903c96
Croves <me@crov.es>
2013-02-10 20:11:46 -0200

    Spell and grammar fix

--- core/MY_Controller.php
+++ core/MY_Controller.php
@@ -112,11 +112,12 @@ class MY_Controller extends CI_Controller
         // If $this->view == FALSE, we don't want to load anything
         if ($this->view !== FALSE)
         {
+            // If the browser is a mobile, let' add a '_mobile' sufix in the view name
             if ($this->agent->is_mobile() === TRUE)
-                $view_sufix = '_model';
+                $view_mobile_sufix = '_mobile';
             
             // If $this->view isn't empty, load it. If it isn't, try and guess based on the controller and action name
-            $view = (!empty($this->view)) ? $this->view : $this->router->directory . $this->router->class . '/' . $this->router->method . $view_sufix;
+            $view = (!empty($this->view)) ? $this->view : $this->router->directory . $this->router->class . '/' . $this->router->method . $view_mobile_sufix;
 
             // Load the view into $yield
             $data['yield'] = $this->load->view($view, $this->data, TRUE);
